8147133b28013bbe90035ce809ce3d36c941f6d9
khushChopra <khush.chopra.cse16@itbhu.ac.in>
2019-03-03 08:20:35 +0530

    login/logout done

--- app/src/main/java/com/dewdastech/dewdasshoppartner/MainActivity.java
+++ app/src/main/java/com/dewdastech/dewdasshoppartner/MainActivity.java
@@ -4,8 +4,12 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 import android.widget.TextView;
@@ -13,6 +17,9 @@ import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -20,58 +27,35 @@ import org.w3c.dom.Text;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
     // CONSTANTS
     protected int RC_SIGN_IN = 894;
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        myFirebaseAuth.removeAuthStateListener(myListener);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        myFirebaseAuth.addAuthStateListener(myListener);
-    }
-
     // Views
     protected BottomNavigationView bottom_navigation;
     protected FrameLayout container;
+    protected Toolbar main_toolbar;
+    protected ActionBar main_actionbar;
 
     // Firebase variables
     protected FirebaseAuth myFirebaseAuth;
     protected FirebaseUser myUser;
     protected FirebaseAuth.AuthStateListener myListener;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         // Views initilisation
-        bottom_navigation = findViewById(R.id.bottom_navigation);
-        container = findViewById(R.id.fragment_container);
+        viewsInit();
 
         // Firebase init
-        myFirebaseAuth = FirebaseAuth.getInstance();
-        myListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                if(firebaseAuth.getCurrentUser()==null){
-                    createSignInIntent();
-                }
-                else{
-                    Toast.makeText(MainActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
-                }
-            }
-        };
-
-
-
+        firebaseInit();
 
         // initial fragment option
         getSupportFragmentManager().beginTransaction().replace(container.getId(),new Details()).commit();
@@ -99,6 +83,27 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void viewsInit(){
+        bottom_navigation = findViewById(R.id.bottom_navigation);
+        container = findViewById(R.id.fragment_container);
+        main_toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(main_toolbar);
+    }
+
+    private void firebaseInit(){
+        myFirebaseAuth = FirebaseAuth.getInstance();
+        myListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser()==null){
+                    createSignInIntent();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+    }
 
     public void createSignInIntent() {
         List<AuthUI.IdpConfig> providers = Arrays.asList(
@@ -109,6 +114,7 @@ public class MainActivity extends AppCompatActivity {
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
                         .setAvailableProviders(providers)
+                        .setLogo(R.drawable.header_logo)
                         .build(),
                 RC_SIGN_IN);
     }
@@ -128,6 +134,39 @@ public class MainActivity extends AppCompatActivity {
                 createSignInIntent();
             }
         }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        myFirebaseAuth.removeAuthStateListener(myListener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        myFirebaseAuth.addAuthStateListener(myListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menu_inflator = getMenuInflater();
+        menu_inflator.inflate(R.menu.app_bar_menu,menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.logout_button){
+            myFirebaseAuth.removeAuthStateListener(myListener);
+            AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    Toast.makeText(getApplicationContext(), "You are logged out, you can exit app", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        return true;
     }
 
 }
